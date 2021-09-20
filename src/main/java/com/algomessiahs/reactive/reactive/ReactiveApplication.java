665b358316cd7d5e1bee5942f5d762ab7a858b8b
tarun kurella <tarunkurella@gmail.com>
2021-09-20 11:58:25 +0530

    yo

---
+++ src/main/java/com/algomessiahs/reactive/reactive/ReactiveApplication.java
@@ -0,0 +1,44 @@
+package com.algomessiahs.reactive.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+@RestController
+public class ReactiveApplication {
+
+	final Sinks.Many sink;
+	final AtomicLong counter;
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+
+
+	public ReactiveApplication() {
+		this.sink = Sinks.many().multicast().onBackpressureBuffer();
+		this.counter = new AtomicLong();
+	}
+
+	@GetMapping("/send")
+	public void test() {
+		Sinks.EmitResult result = sink.tryEmitNext("Hello World #" + counter.getAndIncrement());
+
+		if (result.isFailure()) {
+			// do something here, since emission failed
+		}
+	}
+
+	@GetMapping(value="get",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<ServerSentEvent> sse() {
+		return sink.asFlux().map(e -> ServerSentEvent.builder(e).build());
+	}
+}
---
+++ src/test/java/com/algomessiahs/reactive/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algomessiahs.reactive.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
